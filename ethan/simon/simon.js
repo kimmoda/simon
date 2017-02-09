01625a4bcd9deb734235bb94bfea144a40a94461
Bossface <ejkcat1@gmail.com>
2017-02-08 16:56:23 -0800

    Moved things and changed setTimeout to setInterval

--- ethan/simon/simon.js
+++ ethan/simon/simon.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
   var colors = ["green", "red", "blue", "yellow"];
   var levels = [];
   var waitinSeconds = 2;
@@ -13,21 +12,23 @@ $(document).ready(function () {
 
           console.log(levels);
 
-          levels.forEach(function(entry, colors){
           sleep(waitinSeconds);
-          $("#" + entry + "button").fadeOut(500);
-          $("#" + entry + "button").fadeIn(500);
-        })
       }
 
     $("#" + "green" + "button").click(function() {
+
       levels.push(getRandomColour(colors));
       displaySeries();
+
     });
 
     sleep = function(waitinSeconds){
-      setTimeout(function () {
-      },(waitinSeconds * 3000));
+      levels.forEach(function(entry, colors){
+        setInterval(function () {
+          $("#" + entry + "button").fadeOut(500);
+          $("#" + entry + "button").fadeIn(500);
+        }, 3000);
+      })
     }
 
 });
