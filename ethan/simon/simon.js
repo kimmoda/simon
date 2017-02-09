48113cbaa04e690ea208bbdb2b63061c1c2e54e5
Bossface <ejkcat1@gmail.com>
2017-02-08 16:33:14 -0800

    Removed useless crap

--- ethan/simon/simon.js
+++ ethan/simon/simon.js
@@ -4,37 +4,30 @@ $(document).ready(function () {
   var levels = [];
   var waitinSeconds = 2;
 
-    getRandomColour = function(colors){
-      randomnumber = Math.floor((Math.random() * 4) + 0);
-      return colors[randomnumber];
+      getRandomColour = function(colors){
+        randomnumber = Math.floor((Math.random() * 4) + 0);
+        return colors[randomnumber];
+      }
+
+      displaySeries = function(){
+
+          console.log(levels);
+
+          levels.forEach(function(entry, colors){
+          sleep(waitinSeconds);
+          $("#" + entry + "button").fadeOut(500);
+          $("#" + entry + "button").fadeIn(500);
+        })
+      }
+
+    $("#" + "green" + "button").click(function() {
+      levels.push(getRandomColour(colors));
+      displaySeries();
+    });
+
+    sleep = function(waitinSeconds){
+      setTimeout(function () {
+      },(waitinSeconds * 3000));
     }
-
-
-    displaySeries = function(){
-
-      console.log(levels);
-
-        console.log("Waiting for " + waitinSeconds + " seconds...");
-        console.log(levels);
-
-        levels.forEach(function(entry, colors){
-
-        sleep(waitinSeconds);
-        $("#" + entry + "button").fadeOut(500);
-        $("#" + entry + "button").fadeIn(500);
-        console.log("Wait complete.");
-
-      })
-    }
-
-  $("#" + "green" + "button").click(function() {
-    levels.push(getRandomColour(colors));
-    displaySeries();
-  });
-
-  sleep = function(waitinSeconds){
-    setTimeout(function () {
-    },waitinSeconds * 300);
-  }
 
 });
